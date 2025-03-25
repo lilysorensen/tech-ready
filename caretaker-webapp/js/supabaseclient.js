668bc603146f354429bfb4993ae27735fa7b71bf
Thomaselucas <thomaselucas2020@gmail.com>
2025-03-25 17:58:59 -0600

    finished the supabase client

--- caretaker-webapp/js/supabaseclient.js
+++ caretaker-webapp/js/supabaseclient.js
@@ -1,10 +1,17 @@
+// supabaseclient.js
+import 'dotenv/config';
 
 import  {
     createClient
 } from "https://esm.sh/@supabase/supabase-js";
 
-const supabase = createClient(
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const databaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
+const supabase = createClient(
+    databaseUrl,
+    supabaseKey
 );
+
 
 export { supabase };
